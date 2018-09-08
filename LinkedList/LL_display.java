21060f0de10382e2e9298406f826337acd03e010
sjsid9 <sjsid9@gmail.com>
2018-09-08 13:09:06 +0530

    LL_getAtFunction

--- LinkedList/LL_display.java
+++ LinkedList/LL_display.java
@@ -20,4 +20,26 @@ public void display() {
 		System.out.println("-------------------------------");
 
 	}
+
+public int getAt(int idx) throws Exception {
+
+		if (this.size == 0) {
+			throw new Exception("LL is Empty");
+		}
+
+		if (idx < 0 || idx >= size) {
+			throw new Exception("Invalid Index");
+		}
+
+		Node temp = this.head;
+
+		for (int i = 0; i < idx; i++) {
+			temp = temp.next;
+		}
+
+		return temp.data;
+
+	}
+
+
 }
