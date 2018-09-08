1d9a08f254e28bafd7185ce6d41545d1a051e829
sjsid9 <sjsid9@gmail.com>
2018-09-08 13:31:47 +0530

    LL_Reverse Data Iteratively

---
+++ LinkedList/LL_ReverseDataIteratively.java
@@ -0,0 +1,20 @@
+	public void ReverseDI() throws Exception {
+
+		int left = 0;
+		int right = this.size - 1;
+
+		while (left < right) {
+
+			Node ln = getNodeAt(left);
+			Node rn = getNodeAt(right);
+
+			int temp = ln.data;
+			ln.data = rn.data;
+			rn.data = temp;
+
+			left++;
+			right--;
+
+		}
+
+	}
