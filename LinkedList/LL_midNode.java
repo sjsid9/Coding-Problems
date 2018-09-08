729655b5edad3834b07dda0fb02b446f06f605f2
sjsid9 <sjsid9@gmail.com>
2018-09-08 14:12:02 +0530

    LL midNode

---
+++ LinkedList/LL_midNode.java
@@ -0,0 +1,13 @@
+public Node mid() {
+
+		Node slow = this.head;
+		Node fast = this.head;
+
+		while (fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+		// return slow.data;
+
+	}
