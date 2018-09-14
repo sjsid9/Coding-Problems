9a1456585193d28b15d69c91d2292b6d44134678
sjsid9 <sjsid9@gmail.com>
2018-09-15 01:54:32 +0530

    Count MazePath

---
+++ Strings/CountMazePath.java
@@ -0,0 +1,24 @@
+
+public class CountMazePath {
+	public static void main(String[] args) {
+
+		System.out.println(printMazePath(0, 0, 2, 2));
+
+	}
+
+	private static int printMazePath(int cr, int cc, int er, int ec) {
+
+		if (cr == er && cc == ec) {
+			return 1;
+		}
+
+		if (cr > er || cc > ec) {
+			return 0;
+		}
+
+		int h = printMazePath(cr + 1, cc, er, ec);
+		int v = printMazePath(cr, cc + 1, er, ec);
+
+		return h + v;
+	}
+}
