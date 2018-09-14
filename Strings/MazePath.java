e4b4ced0829f4709410940934a92d6896e5abdfd
sjsid9 <sjsid9@gmail.com>
2018-09-15 00:28:33 +0530

    MazePath Problem

---
+++ Strings/MazePath.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class MazePath {
+	public static void main(String[] args) {
+
+		System.out.println(getMazePath(0, 0, 2, 2));
+
+	}
+
+	private static ArrayList<String> getMazePath(int cr, int cc, int er, int ec) {
+
+		if (cr == er && cc == ec) {
+			ArrayList<String> br = new ArrayList<>();
+			br.add("");
+			return br;
+		}
+
+		if (cr > er || cc > ec) {
+			ArrayList<String> br = new ArrayList<>();
+			return br;
+		}
+
+		ArrayList<String> mr = new ArrayList<>();
+
+		ArrayList<String> rrh = getMazePath(cr + 1, cc, er, ec);
+
+		for (String rrhs : rrh) {
+			mr.add("H" + rrhs);
+		}
+
+		ArrayList<String> rrv = getMazePath(cr, cc + 1, er, ec);
+
+		for (String rrvs : rrv) {
+			mr.add("V" + rrvs);
+		}
+
+		return mr;
+	}
+}
