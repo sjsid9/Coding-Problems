448200f406871fe19409895f6dbbbce6a6706170
sid <sjsid9@gmail.com>
2018-11-16 11:27:36 +0530

    BT is Isomorphic or not
    Signed-off-by: sid <sjsid9@gmail.com>

---
+++ Trees/BinaryTree/BT_isIsomorphic.java
@@ -0,0 +1,23 @@
+	public boolean isIsomorphic(BinaryTree ot) {
+		return isIsomorphic(this.root, ot.root);
+	}
+
+	private boolean isIsomorphic(Node n1, Node n2) {
+
+		if (n1 == null && n2 == null) {
+			return true;
+		}
+
+		if (n1 == null || n2 == null) {
+			return false;
+		}
+
+		if (n1.data != n2.data) {
+			return false;
+		}
+
+		return (isIsomorphic(n1.left, n2.left) && isIsomorphic(n1.right, n2.right)
+				|| isIsomorphic(n1.right, n2.left) && isIsomorphic(n1.left, n2.right));
+
+	}
+
