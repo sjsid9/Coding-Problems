685bc5c90ecc9f0e07bc04c7647d85fc3d09e38a
sid <sjsid9@gmail.com>
2019-01-21 04:06:08 +0530

    all basic fn of bst
    Signed-off-by: sid <sjsid9@gmail.com>

---
+++ Trees/Binary Search Tree/BST.java
@@ -0,0 +1,137 @@
+public class BST {
+
+	private class Node {
+		int data;
+		Node left;
+		Node right;
+	}
+
+	private Node root;
+	private int size;
+
+	public BST(int[] in) {
+		this.root = construct(in, 0, in.length - 1);
+	}
+
+	private Node construct(int[] in, int ilo, int ihi) {
+
+		if (ilo > ihi) {
+			return null;
+		}
+
+		int mid = (ilo + ihi) / 2;
+
+		Node nn = new Node();
+		nn.data = in[mid];
+		this.size++;
+
+		nn.left = construct(in, ilo, mid - 1);
+		nn.right = construct(in, mid + 1, ihi);
+
+		return nn;
+
+	}
+
+	public void display() {
+		display(this.root);
+	}
+
+	private void display(Node node) {
+
+		if (node == null) {
+			return;
+		}
+
+		String str = "";
+
+		if (node.left != null) {
+			str += node.left.data;
+		} else {
+			str += ".";
+		}
+
+		str += "->" + node.data + "<-";
+
+		if (node.right != null) {
+			str += node.right.data;
+		} else {
+			str += ".";
+		}
+		System.out.println(str);
+		display(node.left);
+		display(node.right);
+
+	}
+
+	public int size() {
+		return this.size;
+	}
+
+	public int size2() {
+		return size2(this.root);
+	}
+
+	private int size2(Node node) {
+
+		if (node == null) {
+			return 0;
+		}
+
+		int ls = size2(node.left);
+		int rs = size2(node.right);
+
+		return ls + rs + 1;
+
+	}
+
+	public int height() {
+		return height(this.root);
+	}
+
+	private int height(Node node) {
+
+		if (node == null) {
+			return -1;
+		}
+
+		int lh = height(node.left);
+		int rh = height(node.right);
+
+		return Math.max(lh, rh) + 1;
+
+	}
+
+	public int max() {
+		return max(this.root);
+	}
+
+	private int max(Node node) {
+
+		if (node.right == null) {
+			return node.data;
+		}
+
+		return max(node.right);
+
+	}
+
+	public boolean find(int item) {
+		return find(this.root, item);
+	}
+
+	private boolean find(Node node, int item) {
+
+		if (node == null) {
+			return false;
+		}
+
+		if (item > node.data) {
+			return find(node.right, item);
+		} else if (item < node.data) {
+			return find(node.left, item);
+		} else {
+			return true;
+		}
+
+	}
+}
