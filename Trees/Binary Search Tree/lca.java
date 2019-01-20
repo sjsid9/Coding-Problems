48f0ef62168523be164c25b854a8c8b0a830650e
sid <sjsid9@gmail.com>
2019-01-21 04:17:24 +0530

    lowest common ancestor
    Signed-off-by: sid <sjsid9@gmail.com>

---
+++ Trees/Binary Search Tree/lca.java
@@ -0,0 +1,20 @@
+public int lca(int n1, int n2) {
+		return lca(this.root, n1, n2).data;
+	}
+
+	private Node lca(Node node, int n1, int n2) {
+
+		if (node == null) {
+			return null;
+		}
+
+		if (node.data > n1 && node.data > n2) {
+			return lca(node.left, n1, n2);
+		}
+
+		if (node.data > n1 && node.data > n2) {
+			return lca(node.right, n1, n2);
+		}
+
+		return node;
+	}
